1716bfc580c5632dc84c9b1626e2003d6b8f8141
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-31 16:48:29 +0100

    latest update

--- src/app/components/ui/tabs.tsx
+++ src/app/components/ui/tabs.tsx
@@ -18,21 +18,21 @@ function Tabs({
   );
 }
 
-function TabsList({
-  className,
-  ...props
-}: React.ComponentProps<typeof TabsPrimitive.List>) {
-  return (
-    <TabsPrimitive.List
-      data-slot="tabs-list"
-      className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px] flex",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+// function TabsList({
+//   className,
+//   ...props
+// }: React.ComponentProps<typeof TabsPrimitive.List>) {
+//   return (
+//     <TabsPrimitive.List
+//       data-slot="tabs-list"
+//       className={cn(
+//         "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px] flex",
+//         className,
+//       )}
+//       {...props}
+//     />
+//   );
+// }
 
 function TabsTrigger({
   className,
